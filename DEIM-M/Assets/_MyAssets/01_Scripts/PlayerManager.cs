df3598aeb62dfe74df188a16ae280237709b7daa
Álvaro Holguera <correo@laultimapregunta.com>
2026-09-16 11:52:13 +0200

    Rotación simple

--- DEIM-M/Assets/_MyAssets/01_Scripts/PlayerManager.cs
+++ DEIM-M/Assets/_MyAssets/01_Scripts/PlayerManager.cs
@@ -6,9 +6,13 @@ public class PlayerManager : MonoBehaviour
     bool isAlive;
     public float speed;
     [SerializeField] float desplSpeed; //Serializada para poder cambiarla en Unity
+    [SerializeField] float rotationSpeed; //velocidad a la que rotaré, en vueltas por segundo
 
     //Variable que obtendrá el movimiento del joystick en el eje X
     float moveX;
+
+    //Variable que obiene la rotación del RS
+    float rotation;
 
     //Clase creada con el Input Asset
     InputActions inputActions;
@@ -26,12 +30,18 @@ public class PlayerManager : MonoBehaviour
         inputActions.Player.MoveX.performed += ctx => moveX = ctx.ReadValue<float>();
         inputActions.Player.MoveX.canceled += _ => moveX = 0f;
 
+        //Obtenemos la rotación
+        inputActions.Player.Rotate.performed += ctx => rotation = ctx.ReadValue<float>();
+        inputActions.Player.Rotate.canceled += _ => rotation = 0f;
+
 
     }
 
     private void Update()
     {
         transform.Translate(Vector3.right * desplSpeed * moveX * Time.deltaTime);
+
+        transform.Rotate(Vector3.forward * rotation * rotationSpeed * Time.deltaTime * -360);
     }
 
 
